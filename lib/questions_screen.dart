b80b4475cc1d19dea5c3541a789a87adb6c9dec4
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-05-20 17:02:04 -0300

    70. Mapping Lests & Using the Spread Operator

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -29,22 +29,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(
-            answerText: currentQuestion.answers[0],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[1],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[2],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[3],
-            onTap: () {},
-          ),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(
+              answerText: item,
+              onTap: () {},
+            );
+          }),
         ],
       ),
     );
